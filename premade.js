6c262b5a6cb8e9ba5fbfd53fd2378807aa87eb7a
Garrett Smith <19smitgr@users.noreply.github.com>
2018-02-24 10:25:55 -0500

    Delete premade.js

--- premade.js
+++
@@ -1,8 +0,0 @@
-function createTodoHTML(todo) {
-    // create a div (stands for division) in our webpage to store name
-    var todoDiv = document.createElement('li');
-    todoDiv.innerText = todo;
-
-    // add our newly created div to the names section on our webpage
-    document.getElementById('todos').appendChild(todoDiv);
-}
